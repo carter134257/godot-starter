a303bbb370b5a657499f433661312f76d107727a
Carter Johnson <crlegosnake@gmail.com>
2026-03-30 15:55:08 -0600

    murder

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-@export var move_speed := 220.0
-@export var jump_velocity := -350.0
+@export var move_speed := 300.0
+@export var jump_velocity := -393.0
 @export var gravity := 900.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	#var direction := Input.get_axis("move_left", "move_right")
-	#velocity.x = direction * move_speed
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * move_speed
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
